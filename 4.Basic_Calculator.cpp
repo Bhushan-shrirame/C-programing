eb8be8ec9ff9683314cd1fdd1cc209894e2a734f
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-08 17:09:07 +0530

    4

---
+++ 4.Basic_Calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int a;
+  int b;
+  int sum;                              // declaring the varibale
+  
+  cout << "Enter a number \n";
+  cin >> a;                              // to take input
+  cout << "enter a number \n";
+  cin >> b; 
+  
+  sum = a + b;
+  cout << "sum of a and b is " << sum << endl;
+    
+
+    return 0;
+}
