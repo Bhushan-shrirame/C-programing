1d0c17dcf3102c46779d55eaf802884aac69a527
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-08 17:33:02 +0530

    6

---
+++ 6.if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a;
+    int b;
+    cout << "Enter a number \n";
+    cin >> a;                              
+    cout << "enter a number \n";
+    cin >> b;
+    
+    if(a >= b){                      // if statement
+        cout << "a is greater than b";
+    }
+}
+
+// >   - greater than
+// <   - less than 
+// ==  - equal to
+// >=  - greater than  equal to
+// <=  - less than equal to
+// !=  - not equal to
