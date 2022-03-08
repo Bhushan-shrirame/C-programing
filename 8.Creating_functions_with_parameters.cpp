b71f7a389f55a3eb58353ac6258e7f52e16281bc
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-08 19:24:04 +0530

    8

---
+++ 8.Creating_functions_with_parameters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void favoritenumber(int a){                // creating the fuction with parameter a
+    
+    cout << "bhushan favorite is " << a << endl;
+}
+
+int main() {
+    favoritenumber(5);                      // defining or giving value to the parameter
+    return 0;
+}
