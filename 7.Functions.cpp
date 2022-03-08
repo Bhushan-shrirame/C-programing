bcbdb1bf9ebdf771b960a007d82136be7765efeb
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-08 18:00:06 +0530

    7

---
+++ 7.Functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void printSomething(){
+    cout << "printSomething function" <<endl;
+}              
+
+int main() {
+    
+    printSomething();
+    return 0;
+}
+
+//void function  -  this function dont give any output
+// main function shoul be last 
