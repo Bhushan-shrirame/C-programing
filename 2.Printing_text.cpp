2ab14c6b78f7621727f83a57bd910fc1ef878d22
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-08 16:53:03 +0530

    2

---
+++ 2.Printing_text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  cout << "this is to print something" << endl;  // endl - end line
+  cout << "for \n next line ";                   // \n for adding next line
+    
+    return 0;
+}
