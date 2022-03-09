8174e57e3f516083cb66c66f9110ff5965b3983a
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-09 17:30:54 +0530

    13

---
+++ 13.If_statment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int x = 10; 
+   
+    if(x == 10){                             // if(test) { output }
+        cout << "if statment works ";
+    }
+}
