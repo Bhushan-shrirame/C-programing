e72629ffdb8a34d60907f441df32d4bae1b1237c
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-03-10 14:27:56 +0530

    28

---
+++ 28.Arrays.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+   int bhushan[5] = {1,2,3,4,5};         //  arrays[] = {}       arrays can store many value 
+   cout << bhushan[4] << endl; 
+}
